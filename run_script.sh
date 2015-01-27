f0c3926b19782f9928ed2e32081a4257add63091
Ramyad Hadidi <rhadidi@gatech.edu>
2015-01-26 21:57:05 -0500

    DRRIP Results added :D

--- run_script.sh
+++ run_script.sh
@@ -5,6 +5,8 @@ LLCrepl="drrip"
 LLCreplNum=2
 iCount=200
 
+printf "Run on $(date +%F)\n\n" > output.txt 
+
 for ((i=0;i<$WORKLOADS_L;i++)); do
-	echo "/net/tinker/rhadidi6/CRC/bin/CMPsim.usetrace.64 -threads 4 -mix /net/tinker/rhadidi6/CRC/CRC_traces/mix_${WORKLOADS[${i}]}.mix -cache UL3:4096:64:16 -autorewind 1 -icount ${iCount} -o /net/tinker/rhadidi6/CRC/runs/mix_${WORKLOADS[${i}]}_${LLCrepl}.stats -LLCrepl ${LLCreplNum}"
+	/net/tinker/rhadidi6/CRC/bin/CMPsim.usetrace.64 -threads 4 -mix /net/tinker/rhadidi6/CRC/CRC_traces/mix_${WORKLOADS[${i}]}.mix -cache UL3:4096:64:16 -autorewind 1 -icount ${iCount} -o /net/tinker/rhadidi6/CRC/runs/mix_${WORKLOADS[${i}]}_${LLCrepl}.stats -LLCrepl ${LLCreplNum} >> output.txt
 done
